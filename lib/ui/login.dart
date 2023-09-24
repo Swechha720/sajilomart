b6f324633e6fef02c368d39302cefe51562db8bd
nikitathapa12 <thapanikita171@gmail.com>
2023-09-24 13:48:26 +0545

    commit change

---
+++ lib/ui/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginUI extends StatelessWidget {
+  const LoginUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Sajilo Mart")),
+    );
+  }
+}
